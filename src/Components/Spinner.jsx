844f909c0505e02f557427c656eef1a8a3830e19
enzo franco <enzo.franco.004@gmail.com>
2023-02-10 06:48:04 -0300

    commit

---
+++ src/Components/Spinner.jsx
@@ -0,0 +1,11 @@
+import '../StyleSheets/Spinner.css'
+
+const LazyLoadingSpinner = () => {
+    return (
+        <div className="spinner-container">
+            <div className="spinner"></div>
+        </div>
+      );
+}
+
+export default LazyLoadingSpinner;
