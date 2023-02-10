fabf25933a18228d93877c283c31edf19befb46a
enzo franco <enzo.franco.004@gmail.com>
2023-02-10 06:45:15 -0300

    commit

--- src/Components/Information.jsx
+++ src/Components/Information.jsx
@@ -1,5 +1,5 @@
 import { BiRegistered } from 'react-icons/bi'
-import { AiOutlineInstagram, AiOutlineLinkedin } from 'react-icons/ai'
+import { AiFillGithub, AiOutlineInstagram, AiOutlineLinkedin } from 'react-icons/ai'
 import '../StyleSheets/Information.css'
 import { FormattedMessage } from 'react-intl'
 import { useSelector } from 'react-redux'
@@ -10,17 +10,28 @@ const Information = () => {
 
     return(
         <div className='info-container'>
-            <p><BiRegistered/>2023 Movies Package, inc.</p>
-            
-            <div className='dev-info'>
+
+            <div className='info-container-section'>
+                <p><BiRegistered/>2023 Movies Package, inc.</p>
                 <p><FormattedMessage id='developed.by' defaultMessage='Desarrollado por'/> Enzo Franco</p>
-                <a className={theme} href='https://www.instagram.com/enz0franc0/' target='blank'>
-                    <AiOutlineInstagram className='info-icon'/>
-                </a>
-                <a className={theme} href='https://www.linkedin.com/in/enzo-franco/' target='blank'>
-                    <AiOutlineLinkedin className='info-icon'/>  
-                </a>
             </div>
+
+            <div className='info-container-section'>
+                <div>
+                    <a className={theme} href='https://www.instagram.com/enz0franc0/' target='blank'>
+                        <AiOutlineInstagram className='info-icon'/>
+                     </a>
+                    <a className={theme} href='https://www.linkedin.com/in/enzo-franco/' target='blank'>
+                        <AiOutlineLinkedin className='info-icon'/>  
+                    </a>
+                    <a className={theme} href='https://github.com/esEnzofranco/' target='blank'>
+                        <AiFillGithub className='info-icon'/>  
+                    </a>
+                </div>
+                
+                <a href='https://github.com/esEnzofranco/movies-package' target='blank'><FormattedMessage id='repository' defaultMessage='Repositorio de Github' /></a>
+            </div>
+            
         </div>
     )
 }
